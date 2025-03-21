e087bb9a6f4abda7e06dfe1ad09e1ef4df3e1629
Arshkhan <pathanarshkhan@gmail.com>
2025-03-21 14:27:54 +0530

    Made change in folder structure for vercel deplyment

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    rewrites: async () => {
+      return [
+        {
+          source: "/api/:path*",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/:path*"
+              : "/api/",
+        },
+        {
+          source: "/docs",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/docs"
+              : "/api/py/docs",
+        },
+        {
+          source: "/openapi.json",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/openapi.json"
+              : "/api/py/openapi.json",
+        },
+      ];
+    },
+  };
+
+export default nextConfig;
