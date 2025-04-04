25222d6d623d5dcf096ac83f892cfd8179277740
Arshkhan <pathanarshkhan@gmail.com>
2025-04-04 17:01:15 +0530

    Fix Path Imports

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.db import initialize_db
 from routes.note_routes import router
 
-import sys
-import os
-sys.path.append(os.path.dirname(__file__))
 
 initialize_db()
 
